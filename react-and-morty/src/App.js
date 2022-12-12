982ae0176cdcf01bf520f81bf1e1ad817ed5ca47
Gergely Papp <gregpappdev.gmail.com>
2022-12-12 14:01:09 +0100

    Locations listed

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -6,12 +6,26 @@ function App() {
   const characters = useCharacters(1);
   const locations = useLocations(1);
 
+  const listLocations = () => {
+    return locations.results.map((location) => (
+      <div className="locations">
+        <div className="location-name">{location.name}</div>
+        <div className="location-type">{location.type}</div>
+      </div>
+    ));
+  };
+
   console.log("Characters data: ");
   console.log(characters);
   console.log("Locations data: ");
   console.log(locations);
 
-  return <div className="App">Take a look at the console! (F12)</div>;
+  return (
+    <>
+      <div className="App">Take a look at the console! (F12)</div>
+      {listLocations()}
+    </>
+  );
 }
 
 export default App;
