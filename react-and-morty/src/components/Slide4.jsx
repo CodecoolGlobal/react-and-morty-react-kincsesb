ee70aae2d939f489e3b4fa0493c6c97507f692ea
Gergely Papp <gregpappdev@gmail.com>
2022-12-14 19:04:40 +0100

    Spelling corrected on Slide 4

--- react-and-morty/src/components/Slide4.jsx
+++ react-and-morty/src/components/Slide4.jsx
@@ -4,7 +4,7 @@ const Slide4 = () => {
        <div className="slides">
       <h1 className="presentation-h1">Summary of the Week</h1>
       <h2 className="presentation-h2">There is always something to learn</h2>
-      <p className="presentation-p">React is not a simple thing</p>
+      <p className="presentation-p">React is not simple</p>
       <br></br>
       <p className="presentation-p">Branches & frequent commits save life</p>
       </div>
