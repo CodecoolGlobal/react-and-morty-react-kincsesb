327c4fe0bddb580a796f536602bb89142fda9518
Gergely Papp <gregpappdev@gmail.com>
2022-12-14 19:01:35 +0100

    Spelling corrected on on landing page

--- react-and-morty/src/components/LandingPage.jsx
+++ react-and-morty/src/components/LandingPage.jsx
@@ -3,7 +3,7 @@ const LandingPage = () => {
     return (
         <> 
             <div className="landingpage">           
-                <p className="landing-p">The Rick and Morty universe is big with lot of characters and locations.</p>
+                <p className="landing-p">The Rick and Morty universe is big with lots of characters and locations.</p>
                 <p className="landing-p">To learn more about them, please click on one of the buttons above.</p>
             </div>
         </>
